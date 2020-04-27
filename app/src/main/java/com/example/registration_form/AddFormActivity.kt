de7759cdbe5fcc2b5112b460bbb97b61f147ea22
PinJhih <isaacwang29@gmail.com>
2020-04-27 22:05:29 +0800

    feat: Add form to db

--- app/src/main/java/com/example/registration_form/AddFormActivity.kt
+++ app/src/main/java/com/example/registration_form/AddFormActivity.kt
@@ -1,15 +1,20 @@
 package com.example.registration_form
 
 import android.app.Activity
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_add_form.*
 
 class AddFormActivity : AppCompatActivity() {
 
+    lateinit var db: SQLiteDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_form)
+
+        db = FormsDB(this).writableDatabase
 
         btn_add_form.setOnClickListener {
             addForm()
@@ -21,7 +26,27 @@ class AddFormActivity : AppCompatActivity() {
         }
     }
 
-    private fun addForm(){
+    private fun addForm() {
+        val id = "${System.currentTimeMillis()}"
+        val title = "${ed_title.text}"
+        val startNumber = ed_min_num.text.toString().toInt()
+        val endNumber = ed_max_num.text.toString().toInt()
+        var members = ""
+        var status = ""
 
+        for (i in startNumber until endNumber + 1) {
+            members += "$i,"
+            status += "f,"
+        }
+
+        db.execSQL(
+            "INSERT INTO forms(id,title,members,status) VALUES(?,?,?,?)",
+            arrayOf<Any?>(id, title, members, status)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        db.close()
     }
 }
