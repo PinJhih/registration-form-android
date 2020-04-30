cac46c556b3af1636083068e58316855807ed5f7
PinJhih <isaacwang29@gmail.com>
2020-04-30 23:33:12 +0800

    feat: 取得DB中的資料，並以data class儲存

--- app/src/main/java/com/example/registration_form/MainActivity.kt
+++ app/src/main/java/com/example/registration_form/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.registration_form
 
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var db: SQLiteDatabase
+    private val forms = ArrayList<Form>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = FormsDB(this).writableDatabase
 
         btn_start_edit.setOnClickListener {
             val i = Intent(this, AddFormActivity::class.java)
@@ -23,7 +29,48 @@ class MainActivity : AppCompatActivity() {
         upDateList()
     }
 
-    private fun upDateList(){
+    private fun upDateList() {
+        forms.clear()
+        val data = db.rawQuery("SELECT * FROM forms", null)
+        data.moveToFirst()
 
+        for (i in 0 until data.count) {
+            val id = data.getString(0).toLong()
+            val title = data.getString(1)
+            val members = toMemberList(data.getString(2))
+            val status = toStatusList(data.getString(3))
+            val form = Form(id, title, members, status)
+            forms.add(form)
+            data.moveToNext()
+        }
+        data.close()
     }
+
+    private fun toStatusList(status: String): ArrayList<Boolean> {
+        val list = ArrayList<Boolean>()
+        for (i in status) {
+            if (i == 't')
+                list.add(true)
+            else
+                list.add(false)
+        }
+        return list
+    }
+
+    private fun toMemberList(members: String): ArrayList<Int> {
+        val list = ArrayList<Int>()
+        var member = 0
+
+        for (i in members.indices) {
+            if (i + 1 == members.length || members[i] == ',') {
+                list.add(member)
+                member = 0
+            } else {
+                member *= 10
+                member += members[i].toInt() - 48
+            }
+        }
+        return list
+    }
+
 }
