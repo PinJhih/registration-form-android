f887ee4510b6ac735446d45802c4dce3888515e2
PinJhih <isaacwang29@gmail.com>
2020-09-07 19:34:43 +0800

    feat: 加入 ViewModel

---
+++ app/src/main/java/com/example/registration_form/viewmodel/TablesViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.registration_form.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.registration_form.database.TablesDataBase
+import com.example.registration_form.database.TablesRepository
+import com.example.registration_form.model.Table
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TablesViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: TablesRepository
+    val tables : LiveData<List<Table>>
+
+    init {
+        val tablesDao = TablesDataBase.getInstance(application).tableDao()
+        repository = TablesRepository(tablesDao)
+        tables = repository.tables
+    }
+
+    fun insert(table: Table) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(table)
+    }
+
+    fun delete(table: Table) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(table)
+    }
+
+    fun deleteAll() = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteAll()
+    }
+}
